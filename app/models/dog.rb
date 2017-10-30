f60d51252d94dfe8af30ef770f7c6a5e3b28d20d
Dakota Lillie <dakota.lillie@icloud.com>
2017-10-30 11:33:50 -0400

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+end
+
+# This class should have name, breed, and age attributes which can be set on initialization. You should be able to read and write to these attributes. This class should also keep track of each instance of dog created, as well as a class method all to return an array of those instances.
